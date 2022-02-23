0800aa481168140d78b6e3d3542161559195a5d2
laurahohlsiepe <l.hohlsiepe@gmail.com>
2022-02-23 09:37:34 +0100

    commit

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,37 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  let both = 0;
+
+  for (let i = 0; i < strA.length || i < strB.length; i++) {
+    if (i < strA.length) {
+      both = both + strA[i];
+    }
+
+    if (i < strB.length) {
+      both = both + strB[i];
+    }
+  }
+
+  return both.replace(/^0+/, "");
 }
+
+//
+// const splitStrA = strA.split(" ");
+// const splitStrB = strB.split(" ");
+//let endString = 0;
+
+//for (let i = 0; i < splitStrA.length || i < splitStrB.length; i++) {
+//if (i < splitStrA.length) {
+//  endString += splitStrA[i];
+// }
+//if (i < splitStrB.length) {
+//  endString += splitStrB[i];
+// }
+//}
+
+// return endString.join("");
+
+// }
+
+// endString += secondStrA[i] + secondStrB[i];
+
+// i < strA.length || i < strB.length;
